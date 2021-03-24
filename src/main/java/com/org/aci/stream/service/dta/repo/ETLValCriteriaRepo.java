5c460541597d5fd46112c75b46ad998c4dec5f3b
Ananda Ayyasamy <ananda.ayyasamy@aciworldwide.com>
2021-03-24 19:17:49 -0400

    Updated pom version

--- src/main/java/com/org/aci/stream/service/dta/repo/ETLValCriteriaRepo.java
+++ src/main/java/com/org/aci/stream/service/dta/repo/ETLValCriteriaRepo.java
@@ -3,7 +3,6 @@ import com.org.aci.stream.service.model.ETLValCriteria;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.validation.constraints.Size;
 import java.util.List;
 
 @Repository
